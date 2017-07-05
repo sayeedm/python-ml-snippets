39f94bcbe6fefba9c789f463bb367ffb6adb5c5f
SayeedM <dmbterminal@gmail.com>
2017-07-06 02:12:01 +0600

    commented logistic regression iris

--- linear-regression/log-reg-iris-sk.py
+++ linear-regression/log-reg-iris-sk.py
@@ -1,6 +1,7 @@
 '''
     Running Logistic Regression on Iris Dataset and determine
     if a given row is Iris Virginica
+    Simple Binary Classification
 
     Author: SayeedM
     Date: 06-07-2017
@@ -24,6 +25,7 @@ def irisNameToInt(name):
 iris = pd.read_csv('data/iris.data', header = None)
 
 # converting the name to an encoded int
+# it will assign 2 to iris verginica
 iris[4] = iris[4].apply(irisNameToInt)
 
 # converting to numpy array
@@ -33,12 +35,16 @@ iris = iris.as_matrix()
 X = iris[:, 3].reshape(-1, 1)
 y = (iris[:, 4] == 2).reshape(-1, 1)
 
+# running logistic regression
 log_reg = LogisticRegression()
 log_reg.fit(X, y)
 
+# generating sample inputs
 X_new = np.linspace(0, 3, 10).reshape(-1, 1)
 
+# getting the probabilities
 y_proba = log_reg.predict_proba(X_new)
 
+# now print each inputs False and Truth probability
 for i in range(len(X_new)):
     print("I : ", X_new[i], " F: ", y_proba[i][0], " , T: ", y_proba[i][1])
